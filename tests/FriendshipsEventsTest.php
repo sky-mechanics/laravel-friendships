0cf9a52dfacb36311eaa80a8fc34f7639ac648dc
omatamix <47761650+omatamix@users.noreply.github.com>
2020-04-27 01:41:58 -0400

    CS Fix.

--- tests/FriendshipsEventsTest.php
+++ tests/FriendshipsEventsTest.php
@@ -18,9 +18,12 @@ class FriendshipsEventsTest extends TestCase
     public function friend_request_is_sent()
     {
         Event::fake();
+
         $sender = User::find(1);
         $recipient = User::find(2);
+
         $sender->befriend($recipient);
+
         Event::assertDispatched(Sent::class, function ($event) use ($sender, $recipient) {
             return $event->sender->id === $sender->id && $event->recipient->id === $recipient->id;
         });
@@ -32,9 +35,12 @@ class FriendshipsEventsTest extends TestCase
     public function friend_request_is_accepted()
     {
         Event::fake();
+
         $sender = User::find(1);
         $recipient = User::find(2);
+
         $sender->acceptFriendRequest($recipient);
+
         Event::assertDispatched(Accepted::class, function ($event) use ($sender, $recipient) {
             return $event->sender->id === $sender->id && $event->recipient->id === $recipient->id;
         });
@@ -46,11 +52,14 @@ class FriendshipsEventsTest extends TestCase
     public function friend_request_is_denied()
     {
         Event::fake();
+
         $sender = User::find(1);
         $recipient = User::find(2);
+
         $sender->unfriend($recipient);
         $recipient->befriend($sender);
         $sender->denyFriendRequest($recipient);
+
         Event::assertDispatched(Denied::class, function ($event) use ($sender, $recipient) {
             return $event->sender->id === $sender->id && $event->recipient->id === $recipient->id;
         });
@@ -62,11 +71,14 @@ class FriendshipsEventsTest extends TestCase
     public function friend_is_blocked()
     {
         Event::fake();
+
         $sender = User::find(1);
         $recipient = User::find(2);
+
         $recipient->befriend($sender);
         $sender->acceptFriendRequest($recipient);
         $sender->blockFriend($recipient);
+
         Event::assertDispatched(Blocked::class, function ($event) use ($sender, $recipient) {
             return $event->sender->id === $sender->id && $event->recipient->id === $recipient->id;
         });
@@ -78,14 +90,17 @@ class FriendshipsEventsTest extends TestCase
     public function friend_is_unblocked()
     {
         Event::fake();
+
         $sender = User::find(1);
         $recipient = User::find(2);
+
         $sender->unblockFriend($recipient);
         $sender->unfriend($recipient);
         $recipient->befriend($sender);
         $sender->acceptFriendRequest($recipient);
         $sender->blockFriend($recipient);
         $sender->unblockFriend($recipient);
+
         Event::assertDispatched(Unblocked::class, function ($event) use ($sender, $recipient) {
             return $event->sender->id === $sender->id && $event->recipient->id === $recipient->id;
         });
@@ -97,12 +112,14 @@ class FriendshipsEventsTest extends TestCase
     public function friendship_is_cancelled()
     {
         Event::fake();
+
         $sender = User::find(1);
         $recipient = User::find(2);
         $sender->unfriend($recipient);
         $recipient->befriend($sender);
         $sender->acceptFriendRequest($recipient);
         $sender->unfriend($recipient);
+
         Event::assertDispatched(Cancelled::class, function ($event) use ($sender, $recipient) {
             return $event->sender->id === $sender->id && $event->recipient->id === $recipient->id;
         });
