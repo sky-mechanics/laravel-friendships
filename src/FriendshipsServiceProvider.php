eeae16490f14ace3656501286098d59cb1e3f0c1
omatamix <47761650+omatamix@users.noreply.github.com>
2020-04-27 05:53:22 -0400

    Use correct location for migrations.

--- src/FriendshipsServiceProvider.php
+++ src/FriendshipsServiceProvider.php
@@ -15,8 +15,8 @@ class FriendshipsServiceProvider extends ServiceProvider
     {
         $this->loadMigrationsFrom(__DIR__ . '/../migrations');
         $this->publishes([
-            '0000_00_00_000000_create_friendships_table.stub.php'        => database_path('migrations') . date('Y_m_d_His', time()) . '_create_friendships_table.php',
-            '0000_00_00_000000_create_friendships_groups_table.stub.php' => database_path('migrations') . date('Y_m_d_His', time() + 1) . '_create_friendships_groups_table.php'
+            __DIR__ . '/../migrations/0000_00_00_000000_create_friendships_table.stub.php'        => database_path('migrations') . date('Y_m_d_His', time()) . '_create_friendships_table.php',
+            __DIR__ . '/../migrations/0000_00_00_000000_create_friendships_groups_table.stub.php' => database_path('migrations') . date('Y_m_d_His', time() + 1) . '_create_friendships_groups_table.php'
         ], 'migrations');
         $this->publishes([__DIR__ . '/../config/friendships.php' => config_path('friendships.php')], 'config');
     }
