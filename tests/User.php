0d0bb4903c60ff8e94af6e91074b9ee9b6b42809
omatamix <47761650+omatamix@users.noreply.github.com>
2020-04-30 02:30:48 -0400

    Ordered Imports.

--- tests/User.php
+++ tests/User.php
@@ -4,10 +4,10 @@ namespace Tests;
 
 use Demency\Friendships\Traits\Friendable;
 use Illuminate\Auth\Authenticatable;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\Access\Authorizable;
 
 class User extends Model implements AuthorizableContract,
     AuthenticatableContract
